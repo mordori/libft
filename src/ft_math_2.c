33e136fc8301f8379699a4d89b3e23ac950e6c97
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-07-29 19:41:00 +0300

    fixed norm

--- src/ft_math_2.c
+++ src/ft_math_2.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.c                                          :+:      :+:    :+:   */
+/*   ft_math_2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/03 01:14:46 by myli-pen          #+#    #+#             */
-/*   Updated: 2025/07/29 19:40:03 by myli-pen         ###   ########.fr       */
+/*   Created: 2025/07/29 19:39:37 by myli-pen          #+#    #+#             */
+/*   Updated: 2025/07/29 19:40:11 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_math.h"
 
-float	ft_lerp(float a, float b, float t)
+float	ft_normalize(float value, float min, float max)
 {
-	return ((1.0f - t) * a + t * b);
+	return (ft_clamp01((value - min) / (max - min)));
 }
 
-int	ft_imax(int a, int b)
+float	ft_clamp01(float value)
 {
-	if (a > b)
-		return (a);
-	return (b);
+	return (ft_clamp(value, 0.0f, 1.0f));
 }
 
-int	ft_imin(int a, int b)
+float	ft_clamp(float value, float min, float max)
 {
-	if (a > b)
-		return (b);
-	return (a);
+	if (value <= min)
+		value = min;
+	else if (value >= max)
+		value = max;
+	return (value);
 }
